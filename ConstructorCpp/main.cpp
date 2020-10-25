a8c674048c4482d8e58e6edc009f38f4430f960c
Maciej Walczykowski <maciejwalczykowski@192.168.1.124>
2020-10-26 00:23:16 +0100

    Lab 2 pt 3

--- ConstructorCpp/main.cpp
+++ ConstructorCpp/main.cpp
@@ -2,12 +2,19 @@
 void foo(CoalPowerStation a){
     std::cout << "Hello from foo!" << std::endl;
 }
+void bar(const CoalPowerStation &a){
+    std::cout << "Hello from bar!" << std::endl;
+}
+void qux(const CoalPowerStation &&a){
+    std::cout << "Hello from qux!" << std::endl;
+}
 int main(int argc, char* argv[]){
     CoalPowerStation example(100);
-    example.setMaxCoalOnFeeder(50);
-    example.setEfficiency(0.87);
-    example.setIsActive(true);
+    //example.setMaxCoalOnFeeder(50);
+    //example.setEfficiency(0.87);
+    //example.setIsActive(true);
+    //zrobic to samo co z emp
     example.hireEmployees(10);
-    foo(example);
+    bar(std::move(example));
     return 0;
 }
