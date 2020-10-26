cca24b2d3858d681b7e6f0a81a3fe707f92f20d7
Maciej Walczykowski <maciejwalczykowski@192.168.1.124>
2020-10-26 18:46:52 +0100

    Lab 3 pt 2

--- RecyclingJava/recycling/Wezel.java
+++ RecyclingJava/recycling/Wezel.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Wezel{
+    private ArrayList<Wezel> poloczenia = new ArrayList<>();
     @Deprecated
     @Override
     protected void finalize() throws Throwable{
         System.out.println("Finalizowanie_obiektu");
         super.finalize();
     }
+    private void dodajPolaczenie(Wezel x){
+        this.poloczenia.add(x);
+    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         for(int i = 0; i <= 1000000; i++){
-            new Wezel();
+            Wezel w1 = new Wezel();
+            Wezel w2 = new Wezel();
+            w1.dodajPolaczenie(w2);
+            w2.dodajPolaczenie(w1);
         }
         s.nextLine();
         s.close();
